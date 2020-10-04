7c8aa435820352f099631b5e6fa4d18bbd5337da
prince <princevarshney05@gmail.com>
2020-10-04 16:39:35 +0530

    added password in USER TABLE , COMPLETED FEW FUNCTIONALITIES

--- boilerplate.py
+++ boilerplate.py
@@ -5,7 +5,7 @@ class Session:
     def __init__(self):
         self.connection = pymysql.connect(host='localhost',
                              user='root',
-                             password='password',
+                             password='prince',
                              db='UNIVERSITY',
                              charset='utf8mb4',
                              port = 5005,
@@ -13,44 +13,85 @@ class Session:
         self.cursor = self.connection.cursor()
         self.logged_in = False
         self.role = None
+        self.current_user=None
     
     def login_screen(self):
         print("Hello!")
-        print("1. Login")
-        print("2. Sign Up")
+        while True:
+            print("1. Login")
+            print("2. Sign Up")
+            print("3. Exit")
 
-        selection = input()
-        if(selection == "1"):
-            self.login()
-        elif(selection == "2"):
-            self.signup()
-        else:
-            print("Invalid option")
+            selection = input()
+            if(selection == "1"):
+                self.login()
+            elif(selection == "2"):
+                self.signup()
+            elif(selection == "3"):
+                break
+            else:
+                print("Invalid option")
+
+    def user_screen(self):
+        while True:
+            print("1. BEFRIEND")
+            print("2. EXIT")
+            choice = input()
+            if(choice == "1"):
+                self.befriend()
+            elif(choice == "2"):
+                break
+            else:
+                print("Invalid choice")
 
     def login(self):
+        try:
+            UName = input('UserName: ')
+            DNumber = int(input('DNumber'))
+            Pass = input('Password')
+            query = "SELECT Password from `USER` where UserName = '%s' AND DNum = %d" % (UName,DNumber)
+            self.cursor.execute(query)
+            resultset = self.cursor.fetchone()
+            if(not(resultset)):
+                print("You need to sign up first")
+            else:
+                if(resultset['Password'] == Pass):
+                    print("Sucessfully logged in")
+                    self.current_user = [UName,DNumber]
+                    self.user_screen()
+                    
+                else:
+                    print("Authentication failed")
+            
+        except Exception as e:
+            print(e)
         return   
     def signup(self):
-        return
-    def user_screen(self):
-        return
+        self.add_user()
+    
     def admin_screen(self):
         print("Hello!")
-        print("1. Add User")
-        print("2. Add Course")
-        print("3. Add Subject")
-        print("4. Add Language")
+        while True:
+            
+            print("1. Add User")
+            print("2. Add Course")
+            print("3. Add Subject")
+            print("4. Add Language")
+            print("5. Exit")
 
-        selection = input()
-        if(selection == "1"):
-            self.add_user()
-        elif(selection == "2"):
-            self.add_course()
-        elif(selection == "3"):
-            self.add_subject()
-        elif(selection == "4"):
-            self.add_language()
-        else:
-            print("Invalid option") 
+            selection = input()
+            if(selection == "1"):
+                self.add_user()
+            elif(selection == "2"):
+                self.add_course()
+            elif(selection == "3"):
+                self.add_subject()
+            elif(selection == "4"):
+                self.add_language()
+            elif(selection == "5"):
+                break
+            else:
+                print("Invalid option") 
     def add_entry_screen(self):
         return
     def see_available(self):
@@ -72,7 +113,28 @@ class Session:
     def make_post(self):
         return
     def befriend(self):
-        return
+        try:
+            attributes = {
+                "Friend2Name" : "",
+                "Friend2DNum" : ""
+            }
+            for attribute in attributes:
+                    while(attributes[attribute]==""):
+                        if(attribute == 'Friend2DNum'):
+                            attributes[attribute] = int(input(attribute+": "))
+                        else:
+                            attributes[attribute] = input(attribute+": ")
+                        
+                        
+            
+            query = "INSERT INTO `FRIENDS_WITH` (Friend1Name,Friend1DNum,Friend2Name,Friend2DNum) VALUES ('%s',%d,'%s',%d)" % (attributes["Friend2Name"],attributes["Friend2DNum"],self.current_user[0],self.current_user[1])
+            self.cursor.execute(query)
+            self.connection.commit()
+        except Exception as e:
+            
+            print(e)
+            self.ask_user_action(self.befriend)
+        
     def update_interest(self):
         return
 
@@ -94,16 +156,87 @@ class Session:
     def add_course(self):
         return
     def add_subject(self):
-        return
+        try:
+            print('ADDING SUBJECT')
+            
+            attributes = {"SubName":""
+                }
+        
+            for attribute in attributes:
+                while(attributes[attribute]==""):
+                    attributes[attribute] = input(attribute+": ")
+                
+            query = "INSERT INTO `SUBJECT` VALUES ('%s');" % (attributes['SubName']) 
+            self.cursor.execute(query)
+            self.connection.commit()
+        except Exception as e:
+            print(e)
+            self.ask_user_action(self.add_subject)
+
+
+        
     def add_language(self):
-        return
+        try:
+            print("ADDING LANGUAGE")
+            attributes = {
+                'LangCode' : "",
+                'LangName' : ""
+            }
+            
+            for attribute in attributes:
+                while(attributes[attribute]==""):
+                    attributes[attribute] = input(attribute+": ")
+                    if attribute == 'LangCode':
+                        while(len(attributes[attribute]) != 3):
+                            print("LangCode should be of 3 character")
+                            attributes[attribute] = input(attribute+": ")
+            
+            query = "INSERT INTO `LANGUAGE` VALUES ('%s','%s')" % (attributes['LangCode'],attributes['LangName'])
+            self.cursor.execute(query)
+            self.connection.commit()
+        except Exception as e:
+            print(e)
+            self.ask_user_action(self.add_language)
     def add_prerequisite(self):
         return 
+    def ask_user_action(self,fun_name):
+        print("Oops, you entered something wrong or missed something")
+        while(True):
+                print("1. Retry")
+                print("2. Exit")
+                choice = input()
+                if(choice == "1"):
+                    fun_name()
+                    break
+                elif(choice == "2"):
+                    return
+                else:
+                    print("Invalid choice")
+    def main_screen(self):
+        print("WELCOME TO OPEN SOURCE UNIVERSITY")
+        print("1. ADMIN")
+        print("2. USER")
+        print("3. EXIT")
+        choice = input()
+        if(choice == "1"):
+            self.admin_screen()
+            
+        elif(choice == "2"):
+            self.login_screen()
+        elif(choice == "3"):
+            return 0
+            
+        else:
+            print("invalid choice")
+    
+        
+        
 
 def main():
     session = Session()
     while(True):
-        session.admin_screen()
+        if(session.main_screen() == 0):
+            break
 
 if(__name__ == "__main__"):
     
