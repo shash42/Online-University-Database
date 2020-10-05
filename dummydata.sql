a13515b7af74c1bab6d470c2270ee608cab5f7c3
prince <princevarshney05@gmail.com>
2020-10-06 00:12:39 +0530

    dummy data till course related stuff

---
+++ dummydata.sql
@@ -0,0 +1,56 @@
+USE `UNIVERSITY`;
+INSERT INTO `USER` VALUES ("loremipsum",0,"SHASWAT",NULL,"GOEL","2001-01-01","abc@gmail.com","1234");
+INSERT INTO `USER` VALUES ("morningstar",0,"ADITYA",NULL,"HARI","2000-01-01","abc@yahoo.com","12345");
+INSERT INTO `USER` VALUES ("lifeisgreat",0,"PRINCE",NULL,"VARSHNEY","2000-08-05","abc123@gmail.com","123456");
+
+INSERT INTO `LANGUAGE` VALUES ("eng","ENGLISH");
+INSERT INTO `LANGUAGE` VALUES ("fre","FRENCH");
+INSERT INTO `LANGUAGE` VALUES ("ger","GERMAN");
+
+INSERT INTO `SUBJECT` VALUES ("Computer Science");
+INSERT INTO `SUBJECT` VALUES ("Mathematics");
+INSERT INTO `SUBJECT` VALUES ("Humanities");
+
+INSERT INTO `KNOWS` VALUES ("loremipsum",0,"eng","Professional Working");
+INSERT INTO `KNOWS` VALUES ("morningstar",0,"eng","Professional Working");
+INSERT INTO `KNOWS` VALUES ("lifeisgreat",0,"eng","Professional Working");
+INSERT INTO `KNOWS` VALUES ("morningstar",0,"fre","Elementary");
+INSERT INTO `KNOWS` VALUES ("lifeisgreat",0,"ger","Elementary");
+
+INSERT INTO `HAS_INTEREST_IN` VALUES ("loremipsum",0,"Computer Science","Research");
+INSERT INTO `HAS_INTEREST_IN` VALUES ("morningstar",0,"Humanities","Major");
+INSERT INTO `HAS_INTEREST_IN` VALUES ("lifeisgreat",0,"Mathematics","Major");
+
+INSERT INTO `FRIENDS_WITH` (Friend1Name,Friend1DNum,Friend2Name,Friend2DNum) VALUES ("loremipsum",0,"morningstar",0);
+INSERT INTO `FRIENDS_WITH` (Friend1Name,Friend1DNum,Friend2Name,Friend2DNum) VALUES ("morningstar",0,"lifeisgreat",0);
+
+INSERT INTO `COURSE_DIFFICULTY` VALUES
+("LINEAR ALGEBRA","MIT-OCW","YOUTUBE","Expert"),
+("CULTURE","NPTEL","Swayam","Beginner"),
+("OPEARTING SYSTEM","COURSERA","COURSERA","Intermediate");
+
+
+
+INSERT INTO `COURSE` (`CourseName`, `CourseOrg`, `CoursePlatform`, `CourseHours`, `CourseDuration`) VALUES
+("LINEAR ALGEBRA","MIT-OCW","YOUTUBE",10,30),
+("CULTURE","NPTEL","Swayam",8,25),
+("OPEARTING SYSTEM","COURSERA","COURSERA",15,45);
+
+
+
+INSERT INTO `COURSE_INSTRUCTOR` VALUES
+(1,"XYZ"),
+(1,"PQR"),
+(2,"MNO"),
+(3,"ABC");
+
+INSERT INTO `CONTAINS` VALUES
+("Mathematics",1),
+("Computer Science",1),
+("Humanities",2),
+("Computer Science",3);
+
+
+
+
+
